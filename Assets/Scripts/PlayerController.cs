5aaf8fd0e09dd0811cb7bf29fb90a898422c314e
Thomas A. Anderson <80498637+wrNeo@users.noreply.github.com>
2023-07-13 00:28:24 -0500

    3rd Commit
    Changes.
    
    - Added the scripts
    - The script changes animations
    - Added the Movement on X script
    - Added the Jump Trigger
    - Added the FLIP function (script) when the player walks the other way
    - Added the TAGS and LAYERS
    - Added the Layer GROUND
    - Added the Bool GROUNDED that makes the player change to IDLE when touches the ground.
    
    CHANGES FOR FUTURE:
    1. Tilemap only for Background and Props
    2. Tilemap only for the Borders of the Map
    3. Tilemap only for Platforms
    3.1. Platform Effector to the PlatformsTilemap
    4. Duplicate the same escene to use it like a test subject.
    5. Add the enemies and their animations
    6. Add the items
    7. Add the MAIN MENU and Buttons with Scene Changer Script
    ETC.
    
    Commit Made:
    13.07.2023 - 12:28 a.m (00:28)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 3;
+    public float jumpForce = 10 ;
+
+    private float movX;
+    private float movY;
+
+    private bool facingRight = true;
+    private bool isGrounded = false;
+
+    private Rigidbody2D rb;
+    private Animator _animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        /** Asignación de Movimiento en Horizontal **/
+        movX = Input.GetAxis("Horizontal")*speed;
+        movY = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(movX, rb.velocity.y);
+
+
+        /** Condicion IF de SALTAR con espacio **/
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            rb.AddForce(Vector2.up * jumpForce);
+            /** Condicion IF de SALTAR con espacio **/
+            _animator.SetTrigger("jump");
+        }
+
+        if (movX > 0 && !facingRight)
+        {
+            Flip();
+        }
+        if (movX < 0 && facingRight)
+        {
+            Flip();
+        }
+
+        /** Asignar Animación de Movimiento en X **/
+        _animator.SetFloat("velocityX", Mathf.Abs(movX));
+        _animator.SetFloat("velocityY", rb.velocity.y);
+        _animator.SetBool("grounded", isGrounded);
+
     }
+
+    void Flip()
+    {
+        Vector3 currentScale = transform.localScale;
+        currentScale.x *= -1;
+        transform.localScale = currentScale;
+
+        facingRight = !facingRight;
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+
+        if (other.gameObject.layer == 6)
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        
+
+        if(other.gameObject.layer == 6) 
+        {
+            isGrounded = false;
+        }
+
+
+    }
+
 }
