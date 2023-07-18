0c462a18d64eaf6c0306dc2ad37ae31f1826029a
Thomas A. Anderson <80498637+wrNeo@users.noreply.github.com>
2023-07-18 02:17:51 -0500

    8th Commit
    Still Workin on the Main Menu.
    It is almost done.
    
    Commit Made:
    18.07.2023 - 2:17 am

---
+++ Assets/Scripts/ExitGAME.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGAME : MonoBehaviour
+{
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+
+
+}
