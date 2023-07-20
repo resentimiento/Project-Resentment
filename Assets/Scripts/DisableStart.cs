a3f2dc28206850d94935a58ad22021e6319e8108
Resentimiento <80498637+resentimiento@users.noreply.github.com>
2023-07-20 00:39:05 -0500

    12th Commit
    Few Change:
    
    Added the Script that DISABLES the START Menu and ENABLES the TITTLES Menu
    
    Commit Made:
    20.07.2023 - 00:38 (12:38am)

---
+++ Assets/Scripts/DisableStart.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableStart : MonoBehaviour
+{
+
+    public GameObject startMenu; //Declaramos un objeto de la clase GameObject para START MENU
+    public GameObject tittlesMenu; //Declaramos un objeto de la clase GameObject para TITTLES MENU
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startMenu.SetActive(true);
+        tittlesMenu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            startMenu.SetActive(false);
+            tittlesMenu.SetActive(true);
+        }
+
+    }
+}
